c1fc942927c3c267df3174efdbf6f76baf274b6c
aslansleman512 <aslansleman512@gmail.com>
2025-03-11 00:08:33 +0000

    Add 5.java

---
+++ src/5.java
@@ -0,0 +1,8 @@
+// Java Code
+import java.util.Random;
+public class RandomNumGen {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        System.out.println("Generated Number: " + rand.nextInt());
+    }
+}
