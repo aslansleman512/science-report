f6d25df79b29f7a90f6c8b03c4ca19be418b1243
aslansleman512 <aslansleman512@gmail.com>
2025-03-13 11:23:07 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,6 @@
+public class RandomNumber {
+    public static void main(String[] args) {
+        int number = (int) (Math.random() * 10);
+        System.out.println("Random Number: " + number);
+    }
+}
