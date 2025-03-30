0ab431151c452e4e4492662e3a4740f33e7d3fbb
aslansleman512 <aslansleman512@gmail.com>
2025-03-30 04:30:14 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,6 @@
+public class ScienceReport {
+    public static void main(String[] args) {
+        // Code for your scientific experiment goes here
+        System.out.println("Your report is ready!");
+    }
+}
