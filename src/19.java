a1fb64073823c9d6519f6e1dcf24831eefb5dc1b
aslansleman512 <aslansleman512@gmail.com>
2025-03-31 09:06:46 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,7 @@
+public class ScienceReport {
+
+    public static void main(String[] args) {
+        // Your Java code goes here
+        System.out.println("This is your science report.");
+    }
+}
