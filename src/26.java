794a932d5f92f7849903b9e1770031c01613235d
aslansleman512 <aslansleman512@gmail.com>
2025-04-07 14:30:25 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ScienceReport {
+    public static void main(String[] args) {
+        // Your Java code goes here
+        System.out.println("Hello World!");
+    }
+}
