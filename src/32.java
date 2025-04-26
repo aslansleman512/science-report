819b21dedfc765a56eea267853557df1b1400734
aslansleman512 <aslansleman512@gmail.com>
2025-04-26 15:37:08 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,14 @@
+public class ScienceReport {
+    private int id;
+    private String name;
+    private double score;
+
+    public ScienceReport(int id, String name, double score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+    // Add method for data processing or manipulation
+
+}
